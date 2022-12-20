de03535a71723470c76e117caadc33070c95562e
felinaevangelica <c14210136@john.petra.ac.id>
2022-12-20 11:39:59 +0700

    Update detailProduct.php

--- detailProduct.php
+++ detailProduct.php
@@ -1,19 +1,19 @@
 <?php
     include 'api/connect.php';
     
-    if((!isset($_GET['product_id'])) || empty($_GET['product_id']) || !is_numeric($_GET['product_id'])) {
-        $errors[] = ' ';
-    } else {
-        $product_id = $_GET['product_id'];
+    // if((!isset($_GET['product_id'])) || empty($_GET['product_id']) || !is_numeric($_GET['product_id'])) {
+    //     $errors[] = ' ';
+    // } else {
+    $product_id = $_GET['product_id'];
 
-        $sql = 'SELECT * 
-                FROM product
-                WHERE product_id = ? 
-                LIMIT 1';
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$product_id]);
-        $row = $stmt->fetch();
-    }
+    $sql = 'SELECT * 
+            FROM product
+            WHERE product_id = ? 
+            LIMIT 1';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$product_id]);
+    $row = $stmt->fetch();
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -28,43 +28,65 @@
         <!-- Latest compiled JavaScript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         
-        <title>detail product</title>
+        <title>Detail Product</title>
     </head>
     <body>
         <div class="container-fluid">
             <div class="row">
                 <?php include 'includes/navbar.php'; ?>
             </div>
-
+            <br>
             <div class="row">
                 <div class="col-md-4">
-                    <?php
-                        $stmt = $conn->prepare("SELECT * FROM product WHERE product_id = ?");
-                        $stmt->execute([$product_id]);
-                        while($row = $stmt->fetch()){
-                            echo '<div class="col-md-4 col-sm-6">
-                            <div class="photo">
-                                <img class="card-img-top" src="'.$row['product_img'].'" alt="Card image" style="width:100%;">
-                                <h4 class="card-title">'.$row['product_name'].'</h4>
-                                <p class="card-text">Rp '.$row['product_price'].'</p>
+                    <div class="container border border-dark border-opacity-25 p-5 rounded-1">
+                        <?php
+                        if (isset($_GET['product_id'])){
+                            $stmt = $conn->prepare("SELECT * FROM product WHERE product_id = ?");
+                            $stmt->execute([$product_id]);
+                            while($row = $stmt->fetch()){
+                                // echo '<div class="col-md-4 col-sm-6 p-2">
+                                // <div class="card">';
+                                if ($row['product_img'] == null) {
+                                    echo '<p class="container text-center gy-10 py-5">No Image Available</p>';
+                                }
+                                else {
+                                    echo '<img class="card-img-top" src="'. $row['product_img'] .'" alt="Card image" style="width:100%;">';
+                                };
+                                echo '<div class="card-body">
+                                
                                 </div>
-                            </div>
-                        </div>';
+                                </div>
+                            </div>';
+                            echo $row['product_name'];
+                            echo $row['product_price'];
+
+                            // <h4 class="card-title">'.$row['product_name'].'</h4>
+                            // <p class="card-text">Rp '.$row['product_price'].'</p
+                            }
                         }
-                    ?>
-                </div>
-                <div class="col-md-4">
-
-                </div>
-                <div class="col-md-4">
-
+                        ?>
+                    </div>
                 </div>
             </div>
-
+            <br>
             <div class="row">
-                <h5>Descriptions</h5>
+                <div class="col-md-6">
+                    <h5>Descriptions</h5>
+                    <?php
+                    if (isset($_GET['product_id'])){
+                        $stmt = $conn->prepare("SELECT * FROM product WHERE product_id = ?");
+                        $stmt->execute([$product_id]);
+                        $row = $stmt->fetch();
+                        echo $row['product_size'];
+                    }
+                    ?>
+                    
+                </div>
             </div>
 
         </div>
     </body>
 </html>
+
+<?php
+        
