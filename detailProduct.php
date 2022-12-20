b3ea4c28c9c71fc84dcc900dc40015662d3ea738
felinaevangelica <c14210136@john.petra.ac.id>
2022-12-20 13:36:25 +0700

    Update detailProduct.php

--- detailProduct.php
+++ detailProduct.php
@@ -28,6 +28,25 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         
         <title>Detail Product</title>
+
+        <style>
+            .photo{
+                padding-left : 20px;
+            }
+            .product {
+                padding-left : 30px;
+            }
+
+            hr {
+            display: block;
+            margin-top: 0.5em;
+            margin-bottom: 0.5em;
+            margin-left: auto;
+            margin-right: auto;
+            border-style: inset;
+            border-width: 1px;
+            }
+        </style>
     </head>
     <body>
         <div class="container-fluid">
@@ -35,8 +54,9 @@
                 <?php include 'includes/navbar.php'; ?>
             </div>
             <br>
+
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 photo">
                     <div class="container border border-dark border-opacity-25 rounded-1">
                         <?php
                                 if ($row['product_img'] == null) {
@@ -45,33 +65,51 @@
                                 else {
                                     echo '<img class="card-img-top" src="'. $row['product_img'] .'" alt="Card image" style="width:100%;">';
                                 };
-                                echo '<div class="card-body">
-                                
-                                </div>
-                                </div>
-                            </div>';
-                            
-
-                            // <h4 class="card-title">'.$row['product_name'].'</h4>
-                            // <p class="card-text">Rp '.$row['product_price'].'</p
                         ?>
                     </div>
                 </div>
+
+                <div class="col-md-8 col-sm-8 product">
+                    <div class="row">
+                        <?php
+                        echo '<h4>'.$row['product_name'].'</h4>';
+                        echo '<h4>'.$row['product_price'].'</h4>';
+                        ?>
+                    </div>
+                    <hr>
+
+                    <div class="row">
+                        <h5>Descriptions</h5>
+                        <div>
+                            <?php
+                                echo 'Weight : ' . $row['product_weight'] . '<br>';
+                                echo 'Size : ' . $row['product_size'] . '<br>';
+                                echo 'Describe : ' . $row['product_desc'] . '<br>';
+                                echo 'Category : ' . $row['product_category'];
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="row">
-                <?php
-                echo $row['product_name'];
-                echo $row['product_price'];
-                ?>
-            </div>
+
             <br>
             <div class="row">
-                <div class="col-md-6">
-                    <h5>Descriptions</h5>
-                    <?php
-                        echo 'Size : ' . $row['product_size'];
-                    ?>
-                    
+                <div class="col-md-2">
+                    <div class="container border border-dark border-opacity-25 p-5 rounded-1">
+                            halo
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
                 </div>
             </div>
 
