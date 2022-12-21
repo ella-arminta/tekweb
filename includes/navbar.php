5b923baaa4e5519d25e6418905b543f948571894
C14-ANGEL <c14210129@john.petra.ac.id>
2022-12-21 19:42:41 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,31 +1,39 @@
+<style>
+    .search-button {
+        background-color:#ebd9eb
+    }
+    .search-button:hover {
+        background-color:#8c52ff
+    }
+</style>
 <nav class="navbar navbar-expand-lg navbar fixed-top bg-dark navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-            <img src="resource/img/logo/logo.png" alt="BelaBeli" width="40" height="40">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">BelaBeli.com</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn search-button" type="submit">
-                        <img src="resource/icons/search.png" alt="Search" width="25" height="25">
-                    </button>
-                </form>
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="register.php">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Login</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+        <img src="resource/img/logo/logo.png" alt="BelaBeli" width="40" height="40">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">BelaBeli.com</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn search-button" type="submit">
+                    <img src="resource/icons/search.png" alt="Search" width="25" height="25">
+                </button>
+            </form>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
