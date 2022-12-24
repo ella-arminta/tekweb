3c166f5ae59fd38c1ae3d2ff5208f051867c95f6
C14-ANGEL <c14210129@john.petra.ac.id>
2022-12-24 15:27:20 +0700

    Update index.php
    ini yg versi rapi, lupa push wkwk

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ include 'api/connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
@@ -19,20 +19,8 @@ include 'api/connect.php';
         .content-box {
             width:70%; background-color:#e8907d; margin:auto; margin-top:100px; padding:25px; border-radius:5px
         }
-        .resp-category-button-div {
-            height:150px; width:50px; padding-top:55px;
-        }
-        .button-left {
-            margin-right:20px; float:left
-        }
-        .button-right {
-            margin-left:20px; float:right
-        }
-        .resp-category-button {
-            display:block; margin:auto; width:40px; height:40px
-        }
         .resp-category {
-            height:150px; width:150px; background-color:white; padding-top:10px; border-radius:10px
+            height:150px; width:150px; background-color:white; padding-top:10px; border-radius:10px; margin:auto
         }
         .resp-category:hover {
             border:1px solid #8c52ff;
@@ -59,18 +47,6 @@ include 'api/connect.php';
             display:block; margin:auto; width:165px; height:165px; margin-bottom:5px
         }
         @media only screen and (max-width: 1200px) {
-            .resp-category-button-div {
-                height:120px; width:50px; padding-top:40px;
-            }
-            .button-left {
-                margin-right:15px; float:left
-            }
-            .button-right {
-                margin-left:15px; float:right
-            }
-            .resp-category-button {
-                display:block; margin:auto; width:40px; height:40px
-            }
             .resp-category {
                 height:120px; width:120px; background-color:white; padding-top:10px; border-radius:10px
             }
@@ -98,18 +74,6 @@ include 'api/connect.php';
         @media only screen and (max-width: 900px) {
             .content-box {
                 width:80%; background-color:#e8907d; margin:auto; margin-top:100px; padding:20px; border-radius:5px
-            }
-            .resp-category-button-div {
-                height:100px; width:40px; padding-top:35px;
-            }
-            .button-left {
-                margin-right:10px; float:left
-            }
-            .button-right {
-                margin-left:10px; float:right
-            }
-            .resp-category-button {
-                display:block; margin:auto; width:30px; height:30px
             }
             .resp-category {
                 height:100px; width:100px; background-color:white; padding-top:10px; border-radius:10px
@@ -141,7 +105,7 @@ include 'api/connect.php';
               
             }
         }
-        .justify-content-between{
+        .justify-content-between {
             flex-wrap:wrap;
         }
     </style>
@@ -150,7 +114,7 @@ include 'api/connect.php';
     <?php include "includes/navbar.php"?>
     <div class="content-box">
         <h1 style="font-size:18pt; margin-bottom:25px; color:black">Shop by Category</h1>
-        <div id="multiple-items">
+        <div id="multiple-items" style="width:calc(100% - 45px); margin:auto">
             <div>
                 <div class="resp-category" onclick="window.location.href='category.php'">
                     <img src="resource/icons/electronics.png" alt="Electronics" class="resp-category-img">
